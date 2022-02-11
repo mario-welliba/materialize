537a1d8f8bef52806b4176141cfd54f5800fbb04
mario-welliba <80846534+mario-welliba@users.noreply.github.com>
2022-02-11 17:57:32 +0100

    update

--- Redpanda/scores.sh
+++ Redpanda/scores.sh
@@ -2,7 +2,7 @@
 #while [[ true ]] ; do
 
     # Generate orders for all users
-    for user_id in {1..1000} ; do
+    for user_id in {1..1 ; do
         user_id=$(seq 1 100 | sort -R | head -n1)
         score_id=$(seq 1 2 | sort -R | head -n1)
         score=$(seq 70 130 | sort -R | head -n1)        
