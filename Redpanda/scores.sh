cc3b9f54d3ae10303747c394d4e54f107d60eb5b
mario-welliba <mario.martella@welliba.com>
2022-02-12 16:03:27 +0100

    addes volumes

--- Redpanda/scores.sh
+++ Redpanda/scores.sh
@@ -2,12 +2,12 @@
 #while [[ true ]] ; do
 
     # Generate orders for all users
-    for user_id in {1..1000} ; do
+    for user_id in {1..10000} ; do
         user_id=$(seq 1 100 | sort -R | head -n1)
         score_id=$(seq 1 2 | sort -R | head -n1)
         score=$(seq 70 130 | sort -R | head -n1)        
         export PGPASSWORD='postgres'; psql -h 'localhost' -U 'postgres' -d 'scores' -c "INSERT INTO score_store (user_id, score_id, score) VALUES ( ${user_id}, ${score_id}, ${score} );"
-        sleep 0.01
+        #sleep 0.01
     done
 
 #done
